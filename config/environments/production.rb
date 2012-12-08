8bf568b7e6c1e3b0452c4d99d4dcacff67376297
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2012-12-08 23:39:35 +0100

    changed email host settings for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Poodle::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'poodle.mediacube.at' }
+
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
