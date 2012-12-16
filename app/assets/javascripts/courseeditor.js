cbe3fbc0eb3870efb917896872015fcceff0dfc1
Katrin Hewer <katrinh1987@web.de>
2012-12-16 11:43:49 +0100

    update positions after destroy-action

--- app/assets/javascripts/courseeditor.js
+++ app/assets/javascripts/courseeditor.js
@@ -1,44 +1,52 @@
+$( "#lessons" ).sortable({
+  update: function(){
+    update_lesson_position();
+  }
+});
 
+$( "#lessons" ).disableSelection();
 
-   $( "#lessons" ).sortable({
-     update: function(event, ui) {
-	list = $(this);
-
-        neworder = new Array();
-
-        list.find("li").each(function(i){
-	  elem = $(this);
-          elem.find(".position").html(i+1);
-
-	  id = elem.attr("id").replace('lesson_','');
-	  position = elem.find(".position").html();	
-
-	  neworder.push({
-	    'id': id,
-	    'position': position
-	  });
-
-	});
-
-	course_id = $("#courseeditor div:first-child").attr("id");
-         
-	for(i=0;i<=neworder.length;i++){
-          lesson_id = neworder[i].id;
+function update_lesson_position(){
+  
+  list = $("#lessons");  
+  neworder = new_positions(list);  
+  
+  course_id = $("#courseeditor div:first-child").attr("id");
+           
+  for(i=0;i<=neworder.length;i++){
+    lesson_id = neworder[i].id;
 	  position = neworder[i].position;
 	  $.ajax({
 	    type: "GET",
-            url: "/courses/"+course_id+"/lessons/"+lesson_id+"/updatePosition",
-            data: {
-              "lesson_id": lesson_id,
-              "course_id": course_id,
-              "position": position
-            }
-         });
+      url: "/courses/"+course_id+"/lessons/"+lesson_id+"/updatePosition",
+      data: {
+        "lesson_id": lesson_id,
+        "course_id": course_id,
+        "position": position
+      }
+    });
 	}
+}
 
-     }
-   });
-   $( "#lessons" ).disableSelection();
+function update_page_position(){
+  list = $("#pages");
+  neworder = new_positions(list); 
+}
+
+function new_positions(list){
+  neworder = new Array();
+  list.find("li").each(function(i){
+    elem = $(this);
+    elem.find(".position").html(i+1);
+    id = elem.attr("id").split("_")[1];
+    position = elem.find(".position").html();	
+    neworder.push({
+      'id': id,
+      'position': position
+    });
+  });
+  return neworder;
+}
 
    
 
