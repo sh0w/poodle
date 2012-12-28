0037ca6dae84d4ef65f6af3e835ab1fddcc031c3
Katrin <katrinh1987@web.de>
2012-12-28 13:02:54 +0100

    update position (resources)

--- app/assets/javascripts/courseeditor.js
+++ app/assets/javascripts/courseeditor.js
@@ -25,6 +25,15 @@ function update_page_position(){
   do_requests(url, neworder); 
 }
 
+function update_resource_position(id){
+  var list = $("#page_"+id+" #resources");
+  var neworder = new_positions(list); 
+  var course_id = $("#courseeditor div:first-child").attr("id");
+  var lesson_id = $("#pageeditor div:first-child").attr("id");
+  var url = "/courses/"+course_id+"/lessons/"+lesson_id+"/pages/"+id+"/resources/"; 
+  do_requests(url, neworder);
+}
+
 function new_positions(list){
   var neworder = new Array();
   list.find("> li").each(function(i){
@@ -32,7 +41,6 @@ function new_positions(list){
     var elem = $(this);
     
     var id = elem.attr("id").split("_")[1];
-    
     elem.find(".position").html(i+1);
     var position = elem.find(".position").html();
     
@@ -46,6 +54,7 @@ function new_positions(list){
 }
 
 function do_requests(url, neworder){
+
   for(i=0;i<=neworder.length;i++){
     
     var id = neworder[i].id;
