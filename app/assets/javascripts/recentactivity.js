130f8951458fc8bc14146388b6fee53efa7d3901
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2013-01-06 17:11:17 +0100

    user#show: recent activity looks nicer (included vSlider jquery plugin)

---
+++ app/assets/javascripts/recentactivity.js
@@ -0,0 +1,13 @@
+
+
+$(function() {
+    $('.recent_activity').vTicker({
+        speed: 500,
+        pause: 3000,
+        showItems: 7,
+        animation: 'fade',
+        mousePause: true,
+        height: 0,
+        direction: 'up'
+    });
+});
