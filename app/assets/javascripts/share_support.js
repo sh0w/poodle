4acb4541b6a3c21665a5a3c8078f12cb6961ef0f
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2013-01-13 04:42:21 +0100

    social media

---
+++ app/assets/javascripts/share_support.js
@@ -0,0 +1,18 @@
+
+/* <![CDATA[ */
+        (function() {
+            var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+            t.parentNode.insertBefore(s, t);
+        })();
+
+        /* ]]> */
+(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=519318724754277";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
