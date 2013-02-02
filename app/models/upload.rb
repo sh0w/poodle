5a4c4aef62b5449d92eed91e2e88e83691f16ace
Katrin <katrinh1987@web.de>
2013-02-02 21:58:56 +0100

    Add Resource Labels changed

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,7 +6,7 @@ class Upload < ActiveRecord::Base
   
   has_attached_file :upload
   
-  validates_attachment_size :upload, :less_than => 5.megabytes
+  validates_attachment_size :upload, :less_than => 50.megabytes
   validates_attachment_content_type :upload, :content_type => ['application/pdf', 'application/msword', 'text/plain'], :if => :upload_attached?
 
   def upload_attached?
